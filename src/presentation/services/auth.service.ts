5e7fdee46c6cba4edc45fcf7c4afdb207f7ba65d
christopher <christopher.vorburger2@gmail.com>
2024-09-24 18:33:13 +0200

    Add login in auth service and login dto

--- src/presentation/services/auth.service.ts
+++ src/presentation/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { bcryptAdapter } from "../../config";
 import { UserModel } from "../../data";
 import { CustomError, RegisterUserDto, UserEntity } from "../../domain";
+import { LoginUserDto } from "../../domain/dtos/auth/login-user.dto";
 
 export class AuthService {
   constructor() {}
@@ -25,5 +26,24 @@ export class AuthService {
     } catch (error) {
       throw CustomError.internalServer(`${error}`);
     }
+  }
+
+  public async loginUser(loginUserDto: LoginUserDto) {
+    const user = await UserModel.findOne({
+      email: loginUserDto.email,
+    });
+
+    if (!user) throw CustomError.badRequest("Email not exist");
+
+    const isMatching = bcryptAdapter.compare(
+      loginUserDto.password,
+      user.password
+    );
+
+    if (!isMatching) throw CustomError.badRequest("Password is not valid");
+
+    const { password, ...userEntity } = UserEntity.fromObject(user);
+
+    return { user: userEntity, token: "ABC" };
   }
 }
