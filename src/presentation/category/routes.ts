9b72cb3f320e535ab567e5dfdc0a6c0f7660d232
christopher <christopher.vorburger2@gmail.com>
2024-09-26 16:46:11 +0200

    Add validateJWT function with Auth Middleware

--- src/presentation/category/routes.ts
+++ src/presentation/category/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { CategoryController } from "./controller";
+import { AuthMiddleware } from "../middlewares/auth.middleware";
 
 export class CategoryRoutes {
   static get routes(): Router {
@@ -7,8 +8,7 @@ export class CategoryRoutes {
     const controller = new CategoryController();
 
     router.get("/", controller.getCategory);
-
-    router.post("/", controller.createCategory);
+    router.post("/", [AuthMiddleware.validateJWT], controller.createCategory);
 
     return router;
   }
